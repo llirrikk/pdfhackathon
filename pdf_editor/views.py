57d59d9354291466d5e0ea28c270822aa8f56b9a
ZeeMighty <49806013+ZeeMighty@users.noreply.github.com>
2021-10-23 20:46:29 +0300

    single

--- pdf_editor/views.py
+++ pdf_editor/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.views.generic import View
 from .models import PDFile
 from django.utils import timezone
-from .forms import get_pdf_multiple, merge_form
+from .forms import get_pdf_multiple, merge_form, get_pdf_single
 from django.views.generic.edit import FormView
 from django.core.files import File
 from django.core.files.storage import default_storage
@@ -57,11 +57,11 @@ class MergeNext(View):
 
 class Split(View):
     def get(self, request):
-        form = get_pdf_multiple()
-        return render(request, 'split.html')
+        form = get_pdf_single()
+        return render(request, 'split.html', {'form': form})
 
     def post(self, request):
-        form = get_pdf_multiple(request.POST, request.FILES)
+        form = get_pdf_single(request.POST, request.FILES)
         if form.is_valid():
             form.save()
         return render(request, 'split.html', {'form': form})
@@ -69,11 +69,11 @@ class Split(View):
 
 class Rotate(View):
     def get(self, request):
-        form = get_pdf_multiple()
-        return render(request, 'rotate.html')
+        form = get_pdf_single()
+        return render(request, 'rotate.html', {'form': form})
 
     def post(self, request):
-        form = get_pdf_multiple(request.POST, request.FILES)
+        form = get_pdf_single(request.POST, request.FILES)
         if form.is_valid():
             form.save()
         return render(request, 'rotate.html', {'form': form})
@@ -81,11 +81,11 @@ class Rotate(View):
 
 class Delete(View):
     def get(self, request):
-        form = get_pdf_multiple()
-        return render(request, 'delete.html')
+        form = get_pdf_single()
+        return render(request, 'delete.html', {'form': form})
 
     def post(self, request):
-        form = get_pdf_multiple(request.POST, request.FILES)
+        form = get_pdf_single(request.POST, request.FILES)
         if form.is_valid():
             form.save()
         return render(request, 'delete.html', {'form': form})
@@ -93,11 +93,11 @@ class Delete(View):
 
 class Convert(View):
     def get(self, request):
-        form = get_pdf_multiple()
-        return render(request, 'convert.html')
+        form = get_pdf_single()
+        return render(request, 'convert.html', {'form': form})
 
     def post(self, request):
-        form = get_pdf_multiple(request.POST, request.FILES)
+        form = get_pdf_single(request.POST, request.FILES)
         if form.is_valid():
             form.save()
         return render(request, 'convert.html', {'form': form})
